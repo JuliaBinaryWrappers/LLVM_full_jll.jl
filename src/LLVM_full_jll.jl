3d7c2e14e995bc3086dc7affe1ccd06806d2567c
jlbuild <juliabuildbot@gmail.com>
2026-02-05 15:37:12 +0000

    LLVM_full_jll build 15.0.7+11

--- src/LLVM_full_jll.jl
+++ src/LLVM_full_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("LLVM_full")
-JLLWrappers.@generate_main_file("LLVM_full", UUID("a3ccf953-465e-511d-b87f-60a6490c289d"))
+JLLWrappers.@generate_main_file("LLVM_full", Base.UUID("a3ccf953-465e-511d-b87f-60a6490c289d"))
 end  # module LLVM_full_jll
