f4db8fd06dadaa675d45ff5cbe1da733e2587e8e
jlbuild <juliabuildbot@gmail.com>
2026-09-17 18:34:25 +0000

    LLVM_full_jll build 23.1.1+3

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -5,11 +5,11 @@ using Zlib_jll
 using Zstd_jll
 JLLWrappers.@generate_wrapper_header("LLVM_full")
 JLLWrappers.@declare_library_product(libclang, "@rpath/libclang.dylib")
-JLLWrappers.@declare_library_product(libclang_cpp, "@rpath/libclang-cpp.dylib")
-JLLWrappers.@declare_library_product(libllvm, "@rpath/libLLVM.dylib")
+JLLWrappers.@declare_library_product(libclang_cpp, "@rpath/libclang-cpp.23.1jl.dylib")
+JLLWrappers.@declare_library_product(libllvm, "@rpath/libLLVM.23.1jl.dylib")
 JLLWrappers.@declare_library_product(liblto, "@rpath/libLTO.dylib")
-JLLWrappers.@declare_library_product(mlir, "@rpath/libMLIR.dylib")
-JLLWrappers.@declare_library_product(mlir_c, "@rpath/libMLIR-C.dylib")
+JLLWrappers.@declare_library_product(mlir, "@rpath/libMLIR.23.1jl.dylib")
+JLLWrappers.@declare_library_product(mlir_c, "@rpath/libMLIR-C.23.1jl.dylib")
 JLLWrappers.@declare_executable_product(clang)
 JLLWrappers.@declare_executable_product(dsymutil)
 JLLWrappers.@declare_executable_product(llc)
@@ -33,7 +33,7 @@ function __init__()
 
     JLLWrappers.@init_library_product(
         libllvm,
-        "lib/libLLVM-20jl.dylib",
+        "lib/libLLVM-23jl.dylib",
         nothing,
     )
 
